b34c46956251e22d98cb2cd43dae2a6495a979af
saraalbieri <sara.albieri@gmail.com>
2026-07-01 09:53:42 +0200

    correzione js

--- script/script.js
+++ script/script.js
@@ -907,7 +907,7 @@ function loadOntologyGraph() {
 
     var nodes_onto = new vis.DataSet([
         // Classe Ontologica
-        { id: 'am:Character', label: 'am:Character\n(Classe)', group: 'coreClass', shape: 'box' },
+        /*{ id: 'am:Character', label: 'am:Character\n(Classe)', group: 'coreClass', shape: 'box' },*/
 
         //serie
         { id: 'wd:Q642', label: 'wd:Q642\n(Naruto)', group: 'baseClass', shape: 'box' },
@@ -927,13 +927,13 @@ function loadOntologyGraph() {
     ]);
 
    var edges_onto = new vis.DataSet([
-        // Correzione: P674 connette l'Opera al Personaggio
+        // Connette l'Opera al Personaggio
         { from: 'wd:Q642', to: 'wd:Q1043344', label: 'wdt:P674 (characters)', arrows: 'to', color: {color: '#607D8B'} },
         { from: 'wd:Q16682047', to: 'wd:Q877964', label: 'wdt:P674 (characters)', arrows: 'to', color: {color: '#607D8B'} },
 
         // Istanziazione (rdf:type)
-        { from: 'wd:Q1043344', to: 'am:Character', label: 'rdf:type', arrows: 'to', dashes: true, color: '#94a3b8' },
-        { from: 'wd:Q877964', to: 'am:Character', label: 'rdf:type', arrows: 'to', dashes: true, color: '#94a3b8' },
+       /* { from: 'wd:Q1043344', to: 'am:Character', label: 'rdf:type', arrows: 'to', dashes: true, color: '#94a3b8' },
+        { from: 'wd:Q877964', to: 'am:Character', label: 'rdf:type', arrows: 'to', dashes: true, color: '#94a3b8' },*/
         
         // Relazioni Itachi
         { from: 'wd:Q1043344', to: 'wd:Q1969230', label: 'animanga:hasGeneralArchetype', arrows: 'to', color: '#10b981' },
